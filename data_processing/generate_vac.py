9728c861fde747aa3d29422c34b2c5793f8891c0
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-01-21 17:08:36 +1100

    function to create n combinations of vacancies for a cif file

--- data_processing/generate_vac.py
+++ data_processing/generate_vac.py
@@ -29,7 +29,15 @@ def create_vacancy(input_file, output_file, num_vac):
     
     print(f"vacancies saved to {output_file}")
 
+def process_vacancies(input_file, vac_loop):
+    filename = os.path.splitext(os.path.basename(input_file))[0]
+    for i in range(vac_loop):
+        output_file = f'{filename}_vac{i}.in'
+        with open(f'{output_file}', 'w') as f:
+            pass
+        create_vacancy(input_file, output_file, i)
+
+
 file_in = "/Users/dp/Desktop/pawsey/pawsey-internship/Crystalline_conf_SiAu/SiAu3_cubic/SiAu3_211.cif"
-file_out = "/Users/dp/Desktop/pawsey/pawsey-internship/Crystalline_conf_SiAu/SiAu3_cubic/test.cif"
-vax = 2
-create_vacancy(file_in,file_out,vax)
+vac_loop = 10 # 1 vac up until 10 vacs
+process_vacancies(file_in, vac_loop)
